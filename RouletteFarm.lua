891004f78a159992758d81c131267eeb672a40ab
Immortlas <80988175+Immortlas@users.noreply.github.com>
2021-03-20 13:33:39 +0300

    Update RouletteFarm.lua

--- RouletteFarm.lua
+++ RouletteFarm.lua
@@ -1,6 +1,6 @@
 script_name('RouletteFarm')
 script_author('DeneKyn')
-script_version("1.1")
+script_version("1.2")
 
 local sampev = require 'lib.samp.events'
 local imgui = require 'mimgui'
@@ -11,7 +11,7 @@ local encoding = require "encoding"
 encoding.default = "CP1251"
 u8 = encoding.UTF8
 
-local new, str, sizeof = imgui.new, ffi.string, ffi.sizeof
+local new = imgui.new
 local renderWindow = new.bool()
 
 local mainIni = inicfg.load({
@@ -39,8 +39,6 @@ local settings = {
 	click_use = false,
 	open_chest = false,
 	open_valentine = false,
-	close_invent = false,
-	open_invent = false,
 	inactivity = 0,
 	change_time_position = false,
 	time_x = mainIni.settings.time_x,
@@ -86,63 +84,63 @@ local newFrame = imgui.OnFrame(
         imgui.SetNextWindowPos(imgui.ImVec2(sizeX / 2, sizeY / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
         imgui.SetNextWindowSize(imgui.ImVec2(250, 270), imgui.Cond.FirstUseEver)
         imgui.Begin("Roulette Farm", renderWindow, imgui.WindowFlags.NoResize)
-        imgui.Text(u8"ßùèêè")
+        imgui.Text(u8"Ящики")
 		imgui.BeginChild("##rullet", imgui.ImVec2(230, 84), true, imgui.WindowFlags.NoScrollbar)
-			if imgui.Checkbox(u8'Îáû÷íûé', chest.common.open) then
+			if imgui.Checkbox(u8'Обычный', chest.common.open) then
 				mainIni.chest_open.common = chest.common.open[0]
 				inicfg.save(mainIni)
 			end
 			imgui.SameLine()
-			imgui.TextQuestion("( ? )", u8"ßùèê, êîòîðûé âûäàåòñÿ çà ïðîõîæäåíèå íà÷àëüíûõ êâåñòîâ")
+			imgui.TextQuestion("( ? )", u8"Ящик, который выдается за прохождение начальных квестов")
 			if chest.common.sec > 0 then
 			imgui.SameLine(160)
 			imgui.Text(string.format("%dm : %ds ", math.floor(chest.common.sec/60), chest.common.sec%60))
 			end
 
-			if imgui.Checkbox(u8'Äîíàòíûé', chest.donate.open) then
+			if imgui.Checkbox(u8'Донатный', chest.donate.open) then
 				mainIni.chest_open.donate = chest.donate.open[0]
 				inicfg.save(mainIni)
 			end
 			imgui.SameLine()
-			imgui.TextQuestion("( ? )", u8"ßùèê, êîòîðûé ïîêóïàåòñÿ â äîíàò-ìåíþ çà 6.000 AZ ìîíåò")
+			imgui.TextQuestion("( ? )", u8"Ящик, который покупается в донат-меню за 6.000 AZ монет")
 			if chest.donate.sec > 0 then
 			imgui.SameLine(160)
 			imgui.Text(string.format("%dm : %ds ", math.floor(chest.donate.sec/60), chest.donate.sec%60))
 			end
 
-			if imgui.Checkbox(u8'Ïëàòèíîâûé', chest.platinum.open) then
+			if imgui.Checkbox(u8'Платиновый', chest.platinum.open) then
 				mainIni.chest_open.platinum = chest.platinum.open[0]
 				inicfg.save(mainIni)
 			end
 			imgui.SameLine()
-			imgui.TextQuestion("( ? )", u8"ßùèê, êîòîðûé âûäàåòñÿ ïîñëå òîãî, êàê âû îòûãðàåòå 360 ÷àñîâ")
+			imgui.TextQuestion("( ? )", u8"Ящик, который выдается после того, как вы отыграете 360 часов")
 			if chest.platinum.sec > 0 then
 			imgui.SameLine(160)
 			imgui.Text(string.format("%dm : %ds ", math.floor(chest.platinum.sec/60), chest.platinum.sec%60))
 			end
 		imgui.EndChild()
 
-		imgui.Text(u8"Íàñòðîéêè")
+		imgui.Text(u8"Настройки")
 		imgui.BeginChild("##settings", imgui.ImVec2(230, settings_y), true, imgui.WindowFlags.NoScrollbar)
-			if imgui.Checkbox(u8'Ñîîáùåíèÿ â ÷àò', settings.chat) then
+			if imgui.Checkbox(u8'Сообщения в чат', settings.chat) then
 				mainIni.settings.chat = settings.chat[0]
 				inicfg.save(mainIni)
 			end
 			imgui.SameLine()
-			imgui.TextQuestion("( ? )", u8"Âûâîä ñîîáùåíè¤, ñâÿçàííûé ñ ðàáîîòé ñêðèïòîì, â ÷àò ñàìïà")
+			imgui.TextQuestion("( ? )", u8"Вывод сообщени¤, связанный с рабоотй скриптом, в чат сампа")
 
-			if imgui.Checkbox(u8'Âðåìÿ äî îòêðûòèÿ', settings.time_on_screen) then
+			if imgui.Checkbox(u8'Время до открытия', settings.time_on_screen) then
 				mainIni.settings.time_on_screen = settings.time_on_screen[0]
 				inicfg.save(mainIni)
 			end
 			imgui.SameLine()
-			imgui.TextQuestion("( ? )", u8"Âûâîä íà ýêðàí âðåìåíè äî îòêðûòèÿ ðóëåòêè")
+			imgui.TextQuestion("( ? )", u8"Вывод на экран времени до открытия рулетки")
 
 			if settings.time_on_screen[0] and settings.enabled then
 				settings_y = 85
 				main_y = 100
-				if imgui.Button(u8"Èçìåíèòü ìåñòîïîëîæåíèå", imgui.ImVec2(200, 20)) then
-					chat_msg(string.format("Íàæìèòå êëàâèøó Space, ÷òîáû ñîõðàíèòü ïîçèöèþ"), 0xFFE4B5)
+				if imgui.Button(u8"Изменить местоположение", imgui.ImVec2(200, 20)) then
+					chat_msg(string.format("Нажмите клавишу Space, чтобы сохранить позицию"), 0xFFE4B5)
 					settings.change_time_position = true
 				end
 			else
@@ -151,7 +149,7 @@ local newFrame = imgui.OnFrame(
 		imgui.EndChild()
 
 		imgui.SetCursorPosX((imgui.GetWindowWidth() - 100) / 2)
-		local close_button_text = settings.enabled and u8"Äåàêòèâèðîâàòü" or u8"Àêòèâèðîâàòü"
+		local close_button_text = settings.enabled and u8"Деактивировать" or u8"Активировать"
 		if imgui.Button(close_button_text, imgui.ImVec2(120, 20)) then
 			start()
 		end
@@ -170,8 +168,7 @@ function start()
 				val.td_time_id = 0
 				val.check_time = true
 			end
-			sampSendChat("/invent")
-			settings.close_invent = true
+			sampSendChat("/invent")			
 		else
 			sampTextdrawDelete(222)
 			chat_msg("Deactivated", 0xFF4040)
@@ -183,10 +180,7 @@ function start()
 			}
 			settings.click_use = false
 			settings.open_chest = false
-			settings.open_valentine = false
-			settings.close_invent = false
-			settings.open_invent = false
-			settings.skip_dialog = false
+			settings.open_valentine = false	
 			settings.inactivity = 0
 		end
 end
@@ -220,38 +214,29 @@ function main()
 			end
 
 		if settings.time_on_screen[0] then
-			sampTextdrawSetString(222, string.format("%s-%d:%d~n~%s-%d:%d~n~%s-%d:%d",
-			chest.common.name, math.floor(chest.common.sec/60), chest.common.sec%60,
-			chest.platinum.name, math.floor(chest.platinum.sec/60), chest.platinum.sec%60,
-			chest.donate.name, math.floor(chest.donate.sec/60), chest.donate.sec%60
-			))
+			sampTextdrawSetString(222, textdraw_string())
+		else
+			sampTextdrawSetString(222, "")
 		end
 
 		if settings.inactivity >= 30 then
 			settings.click_use = false
 			settings.open_chest = false
 			settings.open_valentine = false
-			settings.close_invent = false
-			settings.open_invent = false
 			settings.skip_dialog = false
 			settings.inactivity = 0
 		end
 
 		if settings.enabled and sampIsLocalPlayerSpawned() and not settings.change_time_position then
 			wait(1000)
-
 			if (chest.common.sec == 0 and chest.common.open[0]) or (chest.donate.sec == 0 and chest.donate.open[0]) or (chest.platinum.sec == 0 and chest.platinum.open[0]) or (chest.valentine.sec == 0 and chest.valentine.open[0]) then
 				settings.inactivity = settings.inactivity + 1
 			else
 				settings.inactivity = 0
 			end
 
-			for _, val in pairs(chest) do
-			--wait(300)
-				--chat_msg(string.format("%s %s %s %s %s", tostring(val.check_time), tostring(val.open[0]), tostring(settings.open_chest), tostring(settings.enabled), val.name), 0xFFE4B5)
-				if val.check_time and val.open[0] and not settings.open_chest and settings.enabled then
-					--chat_msg("Lol Kek", 0xFFE4B5)
-
+			for _, val in pairs(chest) do			
+				if val.check_time and val.open[0] and not settings.open_chest and settings.enabled then					
 					if sampTextdrawIsExists(buttons.close_invent) then
 					chat_msg("Close invent", 0xFFE4B5)
 					wait(100)
@@ -275,74 +260,73 @@ function sampev.onShowDialog(dialogId, style, title, button1, button2, text)
 end
 
 function sampev.onShowTextDraw(id, data)
-if settings.enabled and sampIsLocalPlayerSpawned() then
+	if settings.enabled and sampIsLocalPlayerSpawned() then
 
-	if data.modelId == 1353 then  -- Ïëàòèíîâûé ñóíäóê
-		chest.platinum.td_id = id
-		chest.platinum.td_time_id = id + 1
-		if chest.platinum.check_time and chest.platinum.open[0] then
-			get_chest_time(chest.platinum)
-		end
-	end
-
-	if data.modelId == 1240 and tostring(data.zoom) == "0.89999902248383" then -- Ñóíäóê âàëåíòèíà
-		chest.valentine.td_id = id
-		chest.valentine.td_time_id = id + 1
-		if chest.valentine.check_time and chest.valentine.open[0] then
-			get_chest_time(chest.valentine)
-		end
-	end
-
-	if data.modelId == 19613 then -- Ñóíäóê çà 6.000 AZ
-		chest.donate.td_id = id
-		chest.donate.td_time_id = id + 1
-		if chest.donate.check_time and chest.donate.open[0] then
-			get_chest_time(chest.donate)
-		end
-	end
-
-	if data.modelId == 19918 then -- Îáû÷íûé ñóíäóê
-		chest.common.td_id = id
-		chest.common.td_time_id = id + 1
-		if chest.common.check_time and chest.common.open[0] then
-			get_chest_time(chest.common)
-		end
-	end
-
-	if data.text == 'USE' and  data.style == 1 and  data.letterColor == -3355444 then
-		buttons.use = id+1
-		if settings.click_use then
-			lua_thread.create(function() wait(100)
-			settings.click_use = false
-			chat_msg("Click use box", 0xFFE4B5)
-			sampSendClickTextdraw(buttons.use)
-			if settings.open_valentine then
-				settings.click_get_valentnie = true
-			else
-				chat_msg("Close invent ", 0xFFE4B5)
-				sampSendClickTextdraw(buttons.close_invent)
-				settings.open_chest = false
+		if data.modelId == 1353 then  -- Платиновый сундук
+			chest.platinum.td_id = id
+			chest.platinum.td_time_id = id + 1
+			if chest.platinum.check_time and chest.platinum.open[0] then
+				get_chest_time(chest.platinum)
 			end
+		end
+
+		if data.modelId == 1240 and tostring(data.zoom) == "0.89999902248383" then -- Сундук валентина
+			chest.valentine.td_id = id
+			chest.valentine.td_time_id = id + 1
+			if chest.valentine.check_time and chest.valentine.open[0] then
+				get_chest_time(chest.valentine)
+			end
+		end
+
+		if data.modelId == 19613 then -- Сундук за 6.000 AZ
+			chest.donate.td_id = id
+			chest.donate.td_time_id = id + 1
+			if chest.donate.check_time and chest.donate.open[0] then
+				get_chest_time(chest.donate)
+			end
+		end
+
+		if data.modelId == 19918 then -- Обычный сундук
+			chest.common.td_id = id
+			chest.common.td_time_id = id + 1
+			if chest.common.check_time and chest.common.open[0] then
+				get_chest_time(chest.common)
+			end
+		end
+
+		if data.text == 'USE' and  data.style == 1 and  data.letterColor == -3355444 then
+			buttons.use = id+1
+			if settings.click_use then
+				lua_thread.create(function() wait(100)
+				settings.click_use = false
+				chat_msg("Click use box", 0xFFE4B5)
+				sampSendClickTextdraw(buttons.use)
+				if settings.open_valentine then
+					settings.click_get_valentnie = true
+				else
+					chat_msg("Close invent ", 0xFFE4B5)
+					sampSendClickTextdraw(buttons.close_invent)
+					settings.open_chest = false
+				end
+				end)
+			end
+		end
+
+		if data.text == 'CLOSE' and data.style == 2 and  data.letterColor == -1  then
+			buttons.close_invent = id-1
+		end
+
+		if id == buttons.valentine_get and settings.open_valentine then
+			chat_msg("Get and close valentine", 0xFFE4B5)
+			lua_thread.create(function() wait(100)
+			settings.open_valentine = false
+			sampSendClickTextdraw(buttons.valentine_get)
+			wait(200)
+			sampSendClickTextdraw(buttons.valentine_close)
+			settings.open_chest = false
 			end)
 		end
 	end
-
-	if data.text == 'CLOSE' and data.style == 2 and  data.letterColor == -1  then
-		buttons.close_invent = id-1
-	end
-
-	if id == buttons.valentine_get and settings.open_valentine then
-		chat_msg("Get and close valentine", 0xFFE4B5)
-		lua_thread.create(function() wait(100)
-		settings.open_valentine = false
-		sampSendClickTextdraw(buttons.valentine_get)
-		wait(200)
-		sampSendClickTextdraw(buttons.valentine_close)
-		settings.open_chest = false
-		end)
-	end
-
-end
 end
 
 function get_chest_time(box)
@@ -403,13 +387,27 @@ function create_textdraw()
 	end
 end
 
-function open_invent_with_skip_dialog()
-	lua_thread.create(function() wait(100)
+function open_invent_with_skip_dialog()	
 		settings.skip_dialog = true
 		sampSendChat("/stats")
 		while settings.skip_dialog do wait(0) end
-		sampSendChat("/invent")
-	end)
+		sampSendChat("/invent")	
+end
+
+function textdraw_string()
+	local td_text = ""
+	if chest.common.open[0] then
+		td_text = td_text..string.format("%s-%d:%d~n~", chest.common.name, math.floor(chest.common.sec/60), chest.common.sec%60)
+	end
+	
+	if chest.donate.open[0] then
+		td_text = td_text..string.format("%s-%d:%d~n~", chest.donate.name, math.floor(chest.donate.sec/60), chest.donate.sec%60)
+	end
+	
+	if chest.platinum.open[0] then
+		td_text = td_text..string.format("%s-%d:%d~n~", chest.platinum.name, math.floor(chest.platinum.sec/60), chest.platinum.sec%60)
+	end	
+	return td_text
 end
 
 function onScriptTerminate(script, quitGame)
@@ -437,21 +435,21 @@ function autoupdate(json_url, prefix, url)
               lua_thread.create(function(prefix)
                 local dlstatus = require('moonloader').download_status
                 local color = -1
-                sampAddChatMessage((prefix..'Îáíàðóæåíî îáíîâëåíèå. Ïûòàþñü îáíîâèòüñÿ c '..thisScript().version..' íà '..updateversion), color)
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
                 wait(250)
                 downloadUrlToFile(updatelink, thisScript().path,
                   function(id3, status1, p13, p23)
                     if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
-                      print(string.format('Çàãðóæåíî %d èç %d.', p13, p23))
+                      print(string.format('Загружено %d из %d.', p13, p23))
                     elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
-                      print('Çàãðóçêà îáíîâëåíèÿ çàâåðøåíà.')
-                      sampAddChatMessage((prefix..'Îáíîâëåíèå çàâåðøåíî!'), color)
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'Обновление завершено!'), color)
                       goupdatestatus = true
                       lua_thread.create(function() wait(500) thisScript():reload() end)
                     end
                     if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
                       if goupdatestatus == nil then
-                        sampAddChatMessage((prefix..'Îáíîâëåíèå ïðîøëî íåóäà÷íî. Çàïóñêàþ óñòàðåâøóþ âåðñèþ..'), color)
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
                         update = false
                       end
                     end
@@ -461,11 +459,11 @@ function autoupdate(json_url, prefix, url)
               )
             else
               update = false
-              print('v'..thisScript().version..': Îáíîâëåíèå íå òðåáóåòñÿ.')
+              print('v'..thisScript().version..': Обновление не требуется.')
             end
           end
         else
-          print('v'..thisScript().version..': Íå ìîãó ïðîâåðèòü îáíîâëåíèå. Ñìèðèòåñü èëè ïðîâåðüòå ñàìîñòîÿòåëüíî íà '..url)
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
           update = false
         end
       end
